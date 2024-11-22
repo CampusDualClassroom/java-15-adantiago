e9a9a1f71cc7592086b485c1e5cc9dba22f52be0
Adan Tiago Rodriguez <adan.tiago@imatia.com>
2024-11-22 09:23:49 +0100

    Ejercicio resuelto

--- src/main/java/com/campusdual/classroom/FreshMerchandise.java
+++ src/main/java/com/campusdual/classroom/FreshMerchandise.java
@@ -5,4 +5,48 @@ import java.util.Date;
 
 public class FreshMerchandise extends Merchandise {
 
+    private Date expiryDate;
+
+    public FreshMerchandise(String name, String uniqueId, String responsibleId) {
+        super(name, uniqueId, responsibleId);
+    }
+
+    public FreshMerchandise(String name, String uniqueId, String responsibleId, int zone, String area, String shelf, int quantity) {
+        super(name, uniqueId, responsibleId, zone, area, shelf, quantity);
+    }
+
+    public FreshMerchandise(String name, String uniqueId, String responsibleId, int zone, String area, String shelf, int quantity, Date expiryDate) {
+        super(name, uniqueId, responsibleId, zone, area, shelf, quantity);
+        this.expiryDate = expiryDate;
+    }
+
+    @Override
+    public String getSpecificData() {
+        return
+            "Name: " + name +
+            ", Unique id: " + uniqueId +
+            ", Responsible id: " + responsibleId +
+            ", Zone: " + zone +
+            ", Area: " + area +
+            ", Shelf: " + shelf +
+            ", Quantity: " + quantity +
+            ", Expiry date: " + this.getFormattedDate(this.getExpirationDate());
+    }
+
+    public void printSpecificData() {
+        System.out.println(this.getSpecificData());
+    }
+
+    public String getFormattedDate(Date date) {
+        SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy");
+        return sdf.format(date);
+    }
+
+    public Date getExpirationDate() {
+        return this.expiryDate;
+    }
+
+    public void setExpirationDate(Date expiryDate) {
+        this.expiryDate = expiryDate;
+    }
 }
